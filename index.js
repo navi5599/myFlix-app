9383383e7185521884c4367d768c1c2174ef11b1
navi5599 <ivkelava10@gmail.com>
2022-07-08 13:33:47 +0200

    Implemented express framework

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express'),
+  morgan = require('morgan');
+
+const app = express();
+
+let topMovies = [
+  {
+    title:'Venom'
+  },
+  {
+    title:'Inception'
+  },
+  {
+    title:'Untouchable'
+  },
+  {
+    title:'Shutter Island'
+  },
+  {
+    title:'Trainspotting'
+  },
+  {
+    title:'Prisoners'
+  },
+  {
+    title:'Gone Girl'
+  },
+  {
+    title:'Spotlight'
+  },
+  {
+    title:'Seven'
+  },
+  {
+    title:'Fight Club'
+  }
+
+];
+
+app.use(morgan('common'));
+
+app.get('/', (req, res) => {
+  res.send('This is my top 10 movies list');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+// get the documentation.html-not working!!
+app.use(express.static('public'));
+
+// error handling code
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+app.listen(8080, () => {
+  console.log('This app is running on port 8080');
+});
