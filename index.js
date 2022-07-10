f4c340d21ee2078f24d53d19194c9b909a2d9f4c
navi5599 <ivkelava10@gmail.com>
2022-07-10 22:51:00 +0200

    fixed documentation route

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  path = require('path');
 
 const app = express();
 
@@ -47,8 +48,12 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
-// get the documentation.html-not working!!
-app.use(express.static('public'));
+// get the documentation.html with static-not working!!
+//app.use(express.static('public'));
+
+app.get('/documentation', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'documentation.html'));
+});
 
 // error handling code
 app.use((err, req, res, next) => {
