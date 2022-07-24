076826fb27cea4d163bf4c011320666d3c8375e4
navi5599 <ivkelava10@gmail.com>
2022-07-24 21:34:16 +0200

    added authorization

---
+++ auth.js
@@ -0,0 +1,34 @@
+const jwtSecret = 'your_jwt_secret',
+ jwt = require('jsonwebtoken'),
+ passport = require('passport');
+
+require('./passport'); // local passport file
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // Username encoding in the JWT
+    expiresIn: '7d', // This specifies when token expires (7 days)
+    algorithm: 'HS256' // Algorithm used to sign or encode the values of the JWT
+  });
+}
+
+// Post login
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token});
+      });
+    })(req, res);
+  });
+}
